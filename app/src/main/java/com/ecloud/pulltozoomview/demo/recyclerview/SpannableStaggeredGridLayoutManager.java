247e0e8967346ba07530b820cdd4934b101b7130
Trần Anh <trantuananh1996@gmail.com>
2019-10-01 17:28:04 +0700

    Update

---
+++ app/src/main/java/com/ecloud/pulltozoomview/demo/recyclerview/SpannableStaggeredGridLayoutManager.java
@@ -0,0 +1,15 @@
+package com.ecloud.pulltozoomview.demo.recyclerview;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by manishdeora on 14/05/15.
+ */
+public class SpannableStaggeredGridLayoutManager extends RecyclerView.LayoutManager{
+
+
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return null;
+    }
+}
